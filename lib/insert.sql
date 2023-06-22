b2f33bc6ede9e4566f014e219fa20d324b5d818d
SamuelWade <samuelrcwade@gmail.com>
2023-06-22 13:14:42 -0400

    lab completed

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES 
+(1,'Mr. Chocolate',9,'M','Black','calm',1),
+(2,'Rowdy',9,'M','Black','calm',1),
+(3,'Tabitha',9,'M','Black','calm',1),
+(4,'Sergeant Brown',9,'M','Black','calm',1),
+(5,'Melissa',9,'M','Black','calm',1),
+(6,'Grinch',9,'M','Black','calm',1),
+(7,'Wendy',9,'M','Black','calm',1),
+(8,null,9,'M','Black','calm',1);
